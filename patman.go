811e6e379ac073df00c84f7b237e0979724f8310
lucagez <lucagesmundo@yahoo.it>
2023-06-29 12:28:05 +0200

    higher default limit for line buffer

--- patman.go
+++ patman.go
@@ -94,6 +94,8 @@ func Run() {
 		print = handleCustomFormatPrint
 	}
 
+	buf := make([]byte, 0, 64*1024)
+	scanner.Buffer(buf, 1024*1024)
 	for scanner.Scan() {
 		var results [][]string // match, name
 		for _, pipeline := range pipelines {
